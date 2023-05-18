7f7ab35fe7408e127282056d4bb09531a57628e2
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-18 22:00:02 +0700

    Make setupView function

--- app/src/main/java/com/althaaf/fruitarians/ui/login/ForgotPasswordActivity.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/login/ForgotPasswordActivity.kt
@@ -1,12 +1,34 @@
 package com.althaaf.fruitarians.ui.login
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.althaaf.fruitarians.R
+import com.althaaf.fruitarians.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
