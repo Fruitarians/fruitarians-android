4e0de8c7e66efc86c28f12054323c901c345d37d
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-18 18:55:40 +0700

    Make Forgot Password Activity

---
+++ app/src/main/java/com/althaaf/fruitarians/ui/login/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.althaaf.fruitarians.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.althaaf.fruitarians.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
