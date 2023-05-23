9d6bf1410f1adfc878ab9901d56fc5b17d000ef5
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-23 17:56:27 +0700

    make ForgotPasswordViewModel.kt to get data in repository

---
+++ app/src/main/java/com/althaaf/fruitarians/ui/authentication/login/ForgotPasswordViewModel.kt
@@ -0,0 +1,17 @@
+package com.althaaf.fruitarians.ui.authentication.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.althaaf.fruitarians.core.data.network.authentication.AuthRepository
+import com.althaaf.fruitarians.core.data.network.authentication.request.login.SentTokenRequest
+import com.althaaf.fruitarians.core.data.network.authentication.request.login.VerifyPasswordRequest
+import com.althaaf.fruitarians.core.data.network.authentication.response.login.SentTokenResponse
+import com.althaaf.fruitarians.core.data.network.authentication.response.login.VerifyPasswordResponse
+import com.althaaf.fruitarians.core.data.network.retrofit.ApiResult
+
+class ForgotPasswordViewModel(private val authRepository: AuthRepository): ViewModel() {
+
+    fun sentToken(sentTokenRequest: SentTokenRequest) : LiveData<ApiResult<SentTokenResponse>> {
+        return authRepository.sentToken(sentTokenRequest)
+    }
+}
