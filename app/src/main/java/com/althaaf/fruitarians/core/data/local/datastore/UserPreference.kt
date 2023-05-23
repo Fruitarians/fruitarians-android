591f09e8b65431f8a67d24253e04868240f9f599
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-23 18:44:25 +0700

    Added UserModel for get dataStore session

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/local/datastore/UserPreference.kt
@@ -0,0 +1,56 @@
+package com.althaaf.fruitarians.core.data.local.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.althaaf.fruitarians.core.data.local.model.UserModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreference private constructor(private val dataStore: DataStore<Preferences>){
+
+    fun getUser(): Flow<UserModel> {
+        return dataStore.data.map { preferences ->
+            UserModel(
+                accessToken = preferences[ACCESS_TOKEN_KEY] ?: "",
+                tokenType = preferences[TOKEN_TYPE_KEY] ?: "",
+                email = preferences[EMAIL_KEY] ?: "",
+                name = preferences[NAME_KEY] ?: ""
+            )
+        }
+    }
+
+    suspend fun saveUser(user: UserModel) {
+        dataStore.edit { preferences ->
+            preferences[ACCESS_TOKEN_KEY] = user.accessToken
+            preferences[TOKEN_TYPE_KEY] = user.tokenType
+            preferences[EMAIL_KEY] = user.email
+            preferences[NAME_KEY] = user.name
+        }
+    }
+
+    suspend fun clearUser() {
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserPreference? = null
+
+        private val ACCESS_TOKEN_KEY = stringPreferencesKey("accessToken")
+        private val TOKEN_TYPE_KEY = stringPreferencesKey("tokenType")
+        private val EMAIL_KEY = stringPreferencesKey("email")
+        private val NAME_KEY = stringPreferencesKey("name")
+
+        fun getInstance(dataStore: DataStore<Preferences>): UserPreference {
+            return INSTANCE ?: synchronized(this) {
+                val instance = UserPreference(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
