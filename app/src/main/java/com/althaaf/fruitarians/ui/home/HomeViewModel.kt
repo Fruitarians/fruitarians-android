4a28edf7a42288b0e8560e94aeba303d6e8beb68
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-23 18:02:44 +0700

    make HomeViewModel.kt to get data in Home Repository

--- app/src/main/java/com/althaaf/fruitarians/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/home/HomeViewModel.kt
@@ -1,13 +1,21 @@
 package com.althaaf.fruitarians.ui.home
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.althaaf.fruitarians.core.data.local.datastore.UserPreference
+import com.althaaf.fruitarians.core.data.local.model.UserModel
+import com.althaaf.fruitarians.core.data.network.home.HomeRepository
+import com.althaaf.fruitarians.core.di.Injection
+import com.althaaf.fruitarians.ui.splashscreen.SplashScreenViewModel
+import com.althaaf.fruitarians.ui.splashscreen.ViewModelFactory
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val homeRepository: HomeRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun getUserSession(): LiveData<UserModel> {
+        return homeRepository.getUserSession()
     }
-    val text: LiveData<String> = _text
 }
+
