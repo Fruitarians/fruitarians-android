2fc401e1c31b333203802cf47b4c428ce2e330db
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-23 17:46:33 +0700

    build ApiConfig file (retrofit build)

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/network/retrofit/ApiConfig.kt
@@ -0,0 +1,36 @@
+package com.althaaf.fruitarians.core.data.network.retrofit
+
+import com.althaaf.fruitarians.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+
+    companion object {
+
+        private var BASE_URL = "https://test-api-386719.et.r.appspot.com/"
+
+        fun getApiService() : ApiService {
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+
+}
