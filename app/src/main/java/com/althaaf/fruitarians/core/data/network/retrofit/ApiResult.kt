aabab65bbd21b9222a1ab1c9aa644580f5fb690e
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-05-23 17:47:23 +0700

    build ApiResult Sealed Class for state condition

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/network/retrofit/ApiResult.kt
@@ -0,0 +1,12 @@
+package com.althaaf.fruitarians.core.data.network.retrofit
+
+sealed class ApiResult<out R> private constructor() {
+
+    data class Success<out T>(val data: T) : ApiResult<T>()
+
+    data class Error(val error: String): ApiResult<Nothing>()
+
+    object Loading : ApiResult<Nothing>()
+
+    object Empty: ApiResult<Nothing>()
+}
