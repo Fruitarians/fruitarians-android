066d1a032e429174eba30a49601edf2b1abf30d5
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 23:24:48 +0700

    Added DataRecommendation object.

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/local/model/DataRecommendation.kt
@@ -0,0 +1,57 @@
+package com.althaaf.fruitarians.core.data.local.model
+
+import com.althaaf.fruitarians.R
+
+object DataRecommendation {
+    val dataFruitRecommendation = listOf<RecommendationModel>(
+        RecommendationModel(
+            id = 1,
+            name = "Buah Pisang",
+            description = "Ini adalah buah pisang guys",
+            image = R.drawable.pisang
+        ),
+        RecommendationModel(
+            id = 2,
+            name = "Buah Strawberry",
+            description = "Ini adalah buah strawberry guys",
+            image = R.drawable.strawberry
+        ),
+        RecommendationModel(
+            id = 3,
+            name = "Buah Alpukat",
+            description = "Ini adalah buah alpukat guys",
+            image = R.drawable.alpukat
+        ),
+        RecommendationModel(
+            id = 4,
+            name = "Buah Apel",
+            description = "Ini adalah buah apel guys",
+            image = R.drawable.apple
+        ),
+        RecommendationModel(
+            id = 5,
+            name = "Buah Jeruk Nipis",
+            description = "Ini adalah buah jeruk nipis guys",
+            image = R.drawable.lime
+        ),
+        RecommendationModel(
+            id = 6,
+            name = "Buah Mangga",
+            description = "Ini adalah buah mangga guys",
+            image = R.drawable.mangga
+        ),
+        RecommendationModel(
+            id = 7,
+            name = "Buah Jambu",
+            description = "Ini adalah buah jambu guys",
+            image = R.drawable.jambu
+        ),
+        RecommendationModel(
+            id = 8,
+            name = "Buah Jeruk Bali",
+            description = "Ini adalah buah jeruk bali guys",
+            image = R.drawable.jeruk
+        ),
+
+    )
+}
