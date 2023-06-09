4a2613baa4105e950e60e7b8dcb387c6a5e0bb3e
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:16:01 +0700

    Added Parcelable implementation to RecommendationModel for easy data passing between components

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/local/model/RecommendationModel.kt
@@ -0,0 +1,12 @@
+package com.althaaf.fruitarians.core.data.local.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class RecommendationModel(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val image:Int
+): Parcelable
