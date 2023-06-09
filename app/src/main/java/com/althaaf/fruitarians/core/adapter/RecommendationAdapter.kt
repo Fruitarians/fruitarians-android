81b8ecfcf763b02992536a6fed283b5b10aec618
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:15:46 +0700

    Implemented RecommendationAdapter for displaying recommendation items in a RecyclerView

---
+++ app/src/main/java/com/althaaf/fruitarians/core/adapter/RecommendationAdapter.kt
@@ -0,0 +1,35 @@
+package com.althaaf.fruitarians.core.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.althaaf.fruitarians.core.data.local.model.RecommendationModel
+import com.althaaf.fruitarians.databinding.ItemRecommendationBinding
+import com.bumptech.glide.Glide
+
+class RecommendationAdapter(private val listRecommendation: List<RecommendationModel>): RecyclerView.Adapter<RecommendationAdapter.MyViewHolder>() {
+
+    class MyViewHolder(private val binding: ItemRecommendationBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: RecommendationModel) {
+            Glide.with(itemView.context)
+                .load(data.image)
+                .into(binding.imgRecommendation)
+
+            itemView.setOnClickListener {
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemRecommendationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount() = listRecommendation.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = listRecommendation[position]
+        holder.bind(item)
+    }
+}
