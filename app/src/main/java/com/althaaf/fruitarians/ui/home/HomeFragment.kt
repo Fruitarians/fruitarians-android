8448c27247dcb9b8aef37e51a26618683897d480
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 23:13:18 +0700

    Added click listeners and setupRecyclerView function.

--- app/src/main/java/com/althaaf/fruitarians/ui/home/HomeFragment.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/home/HomeFragment.kt
@@ -6,7 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.althaaf.fruitarians.R
+import com.althaaf.fruitarians.core.adapter.MarginItemDecoration
+import com.althaaf.fruitarians.core.adapter.RecommendationAdapter
+import com.althaaf.fruitarians.core.data.local.model.DataRecommendation
 import com.althaaf.fruitarians.core.data.network.authentication.response.login.User
 import com.althaaf.fruitarians.core.helper.HomeViewModelFactory
 import com.althaaf.fruitarians.databinding.FragmentHomeBinding
@@ -30,7 +35,42 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViewModel()
+        setupRecyclerView()
         setupView()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.btnFruitStore.setOnClickListener {
+            val toFruitStoreActivity = HomeFragmentDirections.actionNavigationHomeToFruitStoreActivity()
+            view?.findNavController()?.navigate(toFruitStoreActivity)
+        }
+
+        binding.btnMemberships.setOnClickListener {view ->
+            val toMembershipActivity = HomeFragmentDirections.actionNavigationHomeToMembershipActivity()
+            view?.findNavController()?.navigate(toMembershipActivity)
+        }
+
+        binding.btnArticles.setOnClickListener { view ->
+            val toArticleActivity = HomeFragmentDirections.actionNavigationHomeToArticleActivity()
+            view.findNavController().navigate(toArticleActivity)
+        }
+
+        binding.btnFruitVariant.setOnClickListener { view ->
+            val toFruitVariantActivity = HomeFragmentDirections.actionNavigationHomeToFruitVariantActivity()
+            view.findNavController().navigate(toFruitVariantActivity)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val margin = resources.getDimensionPixelSize(R.dimen.activity_horizontal_margin)
+        val itemDecoration = MarginItemDecoration(margin)
+
+        val adapter = RecommendationAdapter(DataRecommendation.dataFruitRecommendation)
+        binding.rvRecommendation.adapter = adapter
+        binding.rvRecommendation.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvRecommendation.addItemDecoration(itemDecoration)
+        binding.rvRecommendation.setHasFixedSize(true)
     }
 
     private fun setupView() {
