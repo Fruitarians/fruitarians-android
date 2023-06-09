e0f00d9a3b999e939ed2444c611d84d500919965
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 23:55:04 +0700

    Add FruitVariantViewModel for managing fruit variant data

---
+++ app/src/main/java/com/althaaf/fruitarians/ui/fruitvariant/FruitVariantViewModel.kt
@@ -0,0 +1,22 @@
+package com.althaaf.fruitarians.ui.fruitvariant
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.althaaf.fruitarians.core.data.network.dashboard.DashboardRepository
+import com.althaaf.fruitarians.core.data.network.dashboard.fruitvariant.BuahResponse
+import com.althaaf.fruitarians.core.data.network.retrofit.ApiResult
+
+class FruitVariantViewModel(private val dashboardRepository: DashboardRepository): ViewModel() {
+
+    fun getAllFruit(q: String?): LiveData<PagingData<com.althaaf.fruitarians.core.data.network.dashboard.fruitvariant.DataItem>> {
+        return dashboardRepository.getAllBuah(q).cachedIn(viewModelScope)
+    }
+
+    fun getSearchBuah(q: String?): LiveData<ApiResult<BuahResponse>> {
+        return dashboardRepository.getSearchBuah(q)
+    }
+
+}
