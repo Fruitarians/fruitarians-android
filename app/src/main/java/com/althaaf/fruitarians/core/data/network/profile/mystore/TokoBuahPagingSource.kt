1025e975b39d67a5264fbfc6e340c1debaf60e14
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:27:20 +0700

    Added class TokoBuahPagingSource that implements PagingSource to load data for the TokoBuah list.

---
+++ app/src/main/java/com/althaaf/fruitarians/core/data/network/profile/mystore/TokoBuahPagingSource.kt
@@ -0,0 +1,43 @@
+package com.althaaf.fruitarians.core.data.network.profile.mystore
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.althaaf.fruitarians.core.data.local.datastore.UserPreference
+import com.althaaf.fruitarians.core.data.network.retrofit.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+
+class TokoBuahPagingSource(private val apiService: ApiService, private val dataStore: UserPreference): PagingSource<Int, BuahItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BuahItem> {
+        return try {
+            val dataSessionUser = withContext(Dispatchers.IO) {
+                dataStore.getUser().first()
+            }
+
+            val page = params.key ?: INITIAL_PAGE_INDEX
+            val responseData = apiService.getTokoBuah(token = "${dataSessionUser.tokenType} ${dataSessionUser.accessToken}", page = page, size = 2)
+            val responseListData: List<BuahItem> = responseData.buah ?: emptyList()
+
+            LoadResult.Page(
+                data = responseListData,
+                prevKey = if (page == 1) null else page - 1,
+                nextKey = if (responseListData.isEmpty()) null else page + 1
+            )
+        }catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, BuahItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+}
