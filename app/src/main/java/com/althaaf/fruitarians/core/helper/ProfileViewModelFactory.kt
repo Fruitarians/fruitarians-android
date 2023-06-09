5ee11777c25061e13704354fd74c4e918ea4ea1e
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:15:23 +0700

    Added ViewModel instantiation for EditProfileViewModel and ChangePasswordViewModel

--- app/src/main/java/com/althaaf/fruitarians/core/helper/ProfileViewModelFactory.kt
+++ app/src/main/java/com/althaaf/fruitarians/core/helper/ProfileViewModelFactory.kt
@@ -3,10 +3,10 @@ package com.althaaf.fruitarians.core.helper
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.althaaf.fruitarians.core.data.network.home.HomeRepository
 import com.althaaf.fruitarians.core.data.network.profile.ProfileRepository
 import com.althaaf.fruitarians.core.di.Injection
-import com.althaaf.fruitarians.ui.home.HomeViewModel
+import com.althaaf.fruitarians.ui.changepassword.ChangePasswordViewModel
+import com.althaaf.fruitarians.ui.edit_profile.EditProfileViewModel
 import com.althaaf.fruitarians.ui.profile.ProfileViewModel
 
 class ProfileViewModelFactory private constructor(private val profileRepository: ProfileRepository) : ViewModelProvider.NewInstanceFactory() {
@@ -14,6 +14,14 @@ class ProfileViewModelFactory private constructor(private val profileRepository:
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
             return  ProfileViewModel(profileRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(EditProfileViewModel::class.java)) {
+            return  EditProfileViewModel(profileRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(ChangePasswordViewModel::class.java)) {
+            return ChangePasswordViewModel(profileRepository) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
