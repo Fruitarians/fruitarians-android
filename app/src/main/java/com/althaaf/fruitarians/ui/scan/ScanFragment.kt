842dff59be806584b1303f5d0406ceec0bf4039e
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:21:19 +0700

    add setup action and on destroy view

--- app/src/main/java/com/althaaf/fruitarians/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/scan/ScanFragment.kt
@@ -1,24 +1,50 @@
 package com.althaaf.fruitarians.ui.scan
 
+import android.os.Build
 import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.althaaf.fruitarians.R
+import com.althaaf.fruitarians.databinding.FragmentScanBinding
 
 class ScanFragment : Fragment() {
+
+    private lateinit var _binding: FragmentScanBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val window: Window? = activity?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.orange)
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.btnScan.setOnClickListener {
+            val toFruitScanActivity = ScanFragmentDirections.actionNavigationScanToFruitScanActivity()
+            it.findNavController().navigate(toFruitScanActivity)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window? = activity?.window
+            window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+        }
     }
 
 }
