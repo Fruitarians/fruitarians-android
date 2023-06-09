9e3d7f612b97385e1fe58f58ffe7349623f26703
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 23:58:37 +0700

    Add MarginItemDecoration for setting right margin on last item in RecyclerView

---
+++ app/src/main/java/com/althaaf/fruitarians/core/adapter/MarginItemDecoration.kt
@@ -0,0 +1,29 @@
+package com.althaaf.fruitarians.core.adapter
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.althaaf.fruitarians.R
+
+class MarginItemDecoration(private val margin: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = state.itemCount
+
+        // Set margin hanya pada item terakhir
+        if (position == itemCount - 1) {
+            outRect.right = margin
+        }
+    }
+}
+
+
+
+
+
