c02c6a0c19c8834e80e590b01e25e3ce7a0a7234
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 22:46:50 +0700

    Added ArticleActivity with setupViewModel, setupRecyclerView, and setupAction functions.

---
+++ app/src/main/java/com/althaaf/fruitarians/ui/article/ArticleActivity.kt
@@ -0,0 +1,77 @@
+package com.althaaf.fruitarians.ui.article
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.althaaf.fruitarians.R
+import com.althaaf.fruitarians.core.adapter.ArticleListAdapter
+import com.althaaf.fruitarians.core.adapter.RecommendationAdapter
+import com.althaaf.fruitarians.core.data.local.model.DataRecommendation
+import com.althaaf.fruitarians.core.data.network.retrofit.ApiResult
+import com.althaaf.fruitarians.core.helper.HomeViewModelFactory
+import com.althaaf.fruitarians.databinding.ActivityArticleBinding
+import com.althaaf.fruitarians.ui.fruitscan.FruitScanActivity
+
+class ArticleActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArticleBinding
+    private lateinit var articleViewModel: ArticleViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewModel()
+        setupRecyclerView()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.btnBack.setOnClickListener {
+            this.onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        articleViewModel.getArticles().observe(this){
+            it?.let { response ->
+                    when (response) {
+                        is ApiResult.Loading -> {
+                            binding.lottieLoading.visibility = View.VISIBLE
+                        }
+
+                        is ApiResult.Success -> {
+                            binding.lottieLoading.visibility = View.GONE
+                            val adapter = ArticleListAdapter(response.data.result)
+                            binding.rvArticle.adapter = adapter
+                            binding.rvArticle.layoutManager = LinearLayoutManager(this)
+                            binding.rvArticle.setHasFixedSize(true)
+                        }
+
+                        is ApiResult.Error -> {
+                            binding.lottieLoading.visibility = View.GONE
+                            Toast.makeText(this, response.error, Toast.LENGTH_SHORT).show()
+                            Log.d(TAG, response.error)
+                        }
+                        else -> {
+                            Toast.makeText(this, "Failed, try again", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setupViewModel() {
+        articleViewModel = ViewModelProvider(this, HomeViewModelFactory.getInstance(this))[ArticleViewModel::class.java]
+    }
+
+    companion object {
+        private const val TAG = "ArticleActivity"
+    }
+}
