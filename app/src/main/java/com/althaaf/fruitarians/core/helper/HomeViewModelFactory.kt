caac729bde305a8e8bd7f5f7c03f06556f519741
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-09 23:15:57 +0700

    Added ViewModel instances for FruitStore, DetailFruitStore, Article, FruitVariant, and Membership.

--- app/src/main/java/com/althaaf/fruitarians/core/helper/HomeViewModelFactory.kt
+++ app/src/main/java/com/althaaf/fruitarians/core/helper/HomeViewModelFactory.kt
@@ -3,21 +3,41 @@ package com.althaaf.fruitarians.core.helper
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.althaaf.fruitarians.core.data.network.authentication.AuthRepository
-import com.althaaf.fruitarians.core.data.network.home.HomeRepository
+import com.althaaf.fruitarians.core.data.network.dashboard.DashboardRepository
 import com.althaaf.fruitarians.core.di.Injection
-import com.althaaf.fruitarians.ui.authentication.login.ForgotPasswordVerifyViewModel
-import com.althaaf.fruitarians.ui.authentication.login.ForgotPasswordViewModel
-import com.althaaf.fruitarians.ui.authentication.login.LoginViewModel
-import com.althaaf.fruitarians.ui.authentication.register.RegisterViewModel
+import com.althaaf.fruitarians.ui.article.ArticleViewModel
+import com.althaaf.fruitarians.ui.detailfruitstore.DetailFruitStoreViewModel
+import com.althaaf.fruitarians.ui.fruitstore.FruitStoreViewModel
+import com.althaaf.fruitarians.ui.fruitvariant.FruitVariantViewModel
 import com.althaaf.fruitarians.ui.home.HomeViewModel
+import com.althaaf.fruitarians.ui.membership.MembershipViewModel
 
-class HomeViewModelFactory private constructor(private val homeRepository: HomeRepository) : ViewModelProvider.NewInstanceFactory() {
+class HomeViewModelFactory private constructor(private val dashboardRepository: DashboardRepository) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return  HomeViewModel(homeRepository) as T
+            return  HomeViewModel(dashboardRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(FruitStoreViewModel::class.java)) {
+            return FruitStoreViewModel(dashboardRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(DetailFruitStoreViewModel::class.java)) {
+            return DetailFruitStoreViewModel(dashboardRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(ArticleViewModel::class.java)) {
+            return ArticleViewModel(dashboardRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(FruitVariantViewModel::class.java)) {
+            return FruitVariantViewModel(dashboardRepository) as T
+        }
+
+        if (modelClass.isAssignableFrom(MembershipViewModel::class.java)) {
+            return MembershipViewModel(dashboardRepository) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
