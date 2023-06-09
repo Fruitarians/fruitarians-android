9e7077f20ab8c39df7db5e078ca4dd86065f431a
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-10 00:12:20 +0700

    Implemented onResume(), setupGetDataUser() methods in ProfileFragment and button clicl listener

--- app/src/main/java/com/althaaf/fruitarians/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/profile/ProfileFragment.kt
@@ -2,27 +2,38 @@ package com.althaaf.fruitarians.ui.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.althaaf.fruitarians.MainActivity
 import com.althaaf.fruitarians.R
+import com.althaaf.fruitarians.core.data.network.dashboard.Alamat
+import com.althaaf.fruitarians.core.data.network.dashboard.Data
 import com.althaaf.fruitarians.core.data.network.retrofit.ApiResult
 import com.althaaf.fruitarians.core.helper.ProfileViewModelFactory
 import com.althaaf.fruitarians.databinding.FragmentHomeBinding
 import com.althaaf.fruitarians.databinding.FragmentProfileBinding
 import com.althaaf.fruitarians.ui.authentication.login.LoginActivity
+import com.althaaf.fruitarians.ui.edit_profile.EditProfileActivity
+import com.althaaf.fruitarians.ui.mystore.MyStoreActivity
+import com.bumptech.glide.Glide
+import com.google.gson.annotations.SerializedName
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var userDataProfile: Data
+    private var userImg: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +47,139 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViewModel()
+        setupGetDataUser()
         setupAction()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupGetDataUser()
+    }
+
+    private fun setupGetDataUser() {
+        profileViewModel.getAllDataUser().observe(requireActivity()) {
+            it?.let { response ->
+                when(response) {
+                    is ApiResult.Loading -> {
+                        binding.lottieLoading.visibility = View.VISIBLE
+                    }
+
+                    is ApiResult.Success -> {
+                        val roleUser = response.data.data.role
+
+                        if (roleUser == "user") {
+                            userDataProfile = Data(
+                                bergabung = response.data.data.bergabung,
+                                role = response.data.data.role,
+                                jamOperasional = response.data.data.jamOperasional,
+                                name = response.data.data.name,
+                                telepon = response.data.data.telepon,
+                                gambarProfil = null,
+                                id = response.data.data.id,
+                                deskripsi = "",
+                                email = response.data.data.email,
+                                alamat = Alamat(
+                                    kota = response.data.data.alamat.kota,
+                                    negara = response.data.data.alamat.negara,
+                                    deskripsiAlamat = response.data.data.alamat.deskripsiAlamat
+                                )
+                            )
+
+                            userImg = response.data.data.gambarProfil
+
+                            binding.btnMystore.visibility = View.GONE
+                            binding.lottieLoading.visibility = View.GONE
+                            binding.nameProfile.text = response.data.data.name
+                            binding.roleProfile.text = "User"
+                            binding.imgProfile.setImageDrawable(ContextCompat.getDrawable(binding.imgProfile.context, R.drawable.default_user_biasa))
+                            binding.imgProfile.elevation = 6F
+                        } else if (roleUser == "toko") {
+                            userDataProfile = Data(
+                                bergabung = response.data.data.bergabung,
+                                role = response.data.data.role,
+                                jamOperasional = response.data.data.jamOperasional,
+                                name = response.data.data.name,
+                                telepon = response.data.data.telepon,
+                                gambarProfil = response.data.data.gambarProfil,
+                                id = response.data.data.id,
+                                deskripsi = response.data.data.deskripsi,
+                                email = response.data.data.email,
+                                alamat = Alamat(
+                                    kota = response.data.data.alamat.kota,
+                                    negara = response.data.data.alamat.negara,
+                                    deskripsiAlamat = response.data.data.alamat.deskripsiAlamat
+                                )
+                            )
+
+                            userImg = response.data.data.gambarProfil
+
+                            binding.lottieLoading.visibility = View.GONE
+                            binding.nameProfile.text = response.data.data.name
+                            binding.roleProfile.text = "Seller"
+                            binding.btnMystore.visibility = View.VISIBLE
+                            binding.btnMyvendor.visibility = View.GONE
+
+                            if (response.data.data.gambarProfil != null) {
+                                Glide.with(requireActivity())
+                                    .load(response.data.data.gambarProfil)
+                                    .into(binding.imgProfile)
+                                binding.imgProfile.elevation = 6F
+
+                            } else {
+                                binding.imgProfile.setImageDrawable(ContextCompat.getDrawable(binding.imgProfile.context, R.drawable.default_img_toko))
+                                binding.imgProfile.elevation = 6F
+                            }
+                        } else if (roleUser == "vendor") {
+                            userDataProfile = Data(
+                                bergabung = response.data.data.bergabung,
+                                role = response.data.data.role,
+                                jamOperasional = response.data.data.jamOperasional,
+                                name = response.data.data.name,
+                                telepon = response.data.data.telepon,
+                                gambarProfil = response.data.data.gambarProfil,
+                                id = response.data.data.id,
+                                deskripsi = response.data.data.deskripsi,
+                                email = response.data.data.email,
+                                alamat = Alamat(
+                                    kota = response.data.data.alamat.kota,
+                                    negara = response.data.data.alamat.negara,
+                                    deskripsiAlamat = response.data.data.alamat.deskripsiAlamat
+                                )
+                            )
+
+                            userImg = response.data.data.gambarProfil
+
+                            binding.lottieLoading.visibility = View.GONE
+                            binding.nameProfile.text = response.data.data.name
+                            binding.roleProfile.text = "Vendor"
+                            binding.btnMystore.visibility = View.GONE
+                            binding.btnMyvendor.visibility = View.VISIBLE
+
+                            if (response.data.data.gambarProfil != null) {
+                                Glide.with(requireActivity())
+                                    .load(response.data.data.gambarProfil)
+                                    .into(binding.imgProfile)
+                                binding.imgProfile.elevation = 6F
+
+                            } else {
+                                binding.imgProfile.setImageDrawable(ContextCompat.getDrawable(binding.imgProfile.context, R.drawable.default_img_toko))
+                                binding.imgProfile.elevation = 6F
+                            }
+                        }
+                    }
+
+                    is ApiResult.Error -> {
+                        binding.lottieLoading.visibility = View.GONE
+                        Toast.makeText(requireActivity(), response.error, Toast.LENGTH_SHORT).show()
+                    }
+
+                    else -> {
+                        binding.lottieLoading.visibility = View.GONE
+                        Toast.makeText(requireActivity(), "Failed, try again", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 
     private fun setupViewModel() {
@@ -53,7 +196,7 @@ class ProfileFragment : Fragment() {
                     when (response) {
                         is ApiResult.Loading -> {
                             binding.progressBar.visibility = View.VISIBLE
-                            binding.buttonLogout.visibility = View.GONE
+                            binding.buttonLogout.text = ""
                         }
 
                         is ApiResult.Success -> {
@@ -70,7 +213,6 @@ class ProfileFragment : Fragment() {
                             Toast.makeText(requireActivity(), response.error, Toast.LENGTH_SHORT)
                                 .show()
                             binding.progressBar.visibility = View.GONE
-                            binding.buttonLogout.visibility = View.VISIBLE
                         }
                         else -> {
                             Toast.makeText(
@@ -82,6 +224,31 @@ class ProfileFragment : Fragment() {
                     }
                 }
             }
+        }
+
+        binding.btnMystore.setOnClickListener { view->
+            val toMyStoreActivity = ProfileFragmentDirections.actionNavigationProfileToMyStoreActivity()
+            view.findNavController().navigate(toMyStoreActivity)
+        }
+
+        binding.btnMyvendor.setOnClickListener { view->
+            val toMyVendorActivity = ProfileFragmentDirections.actionNavigationProfileToMyVendorActivity()
+            view.findNavController().navigate(toMyVendorActivity)
+        }
+
+        binding.btnLanguage.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+
+        binding.btnChangePassword.setOnClickListener { view ->
+            val toChangePasswordFragment = ProfileFragmentDirections.actionNavigationProfileToChangePasswordFragment(userImg)
+            view.findNavController().navigate(toChangePasswordFragment)
+        }
+
+        binding.btnEditProfile.setOnClickListener { view ->
+            val toEditProfileFragment = ProfileFragmentDirections.actionNavigationProfileToEditProfileFragment(userDataProfile)
+            toEditProfileFragment.detailUser = userDataProfile
+            view.findNavController().navigate(toEditProfileFragment)
         }
     }
 
