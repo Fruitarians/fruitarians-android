3b189956ce439c119ca73bac92297675aaef396d
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-13 18:58:15 +0700

    add search argument in each data class RecommendationModel

--- app/src/main/java/com/althaaf/fruitarians/core/data/local/model/DataRecommendation.kt
+++ app/src/main/java/com/althaaf/fruitarians/core/data/local/model/DataRecommendation.kt
@@ -7,48 +7,56 @@ object DataRecommendation {
         RecommendationModel(
             id = 1,
             name = "Buah Pisang",
+            search = "pisang",
             description = "Ini adalah buah pisang guys",
             image = R.drawable.pisang
         ),
         RecommendationModel(
             id = 2,
             name = "Buah Strawberry",
+            search = "strawberry",
             description = "Ini adalah buah strawberry guys",
             image = R.drawable.strawberry
         ),
         RecommendationModel(
             id = 3,
             name = "Buah Alpukat",
+            search = "alpukat",
             description = "Ini adalah buah alpukat guys",
             image = R.drawable.alpukat
         ),
         RecommendationModel(
             id = 4,
             name = "Buah Apel",
+            search = "apel",
             description = "Ini adalah buah apel guys",
             image = R.drawable.apple
         ),
         RecommendationModel(
             id = 5,
             name = "Buah Jeruk Nipis",
+            search = "lime",
             description = "Ini adalah buah jeruk nipis guys",
             image = R.drawable.lime
         ),
         RecommendationModel(
             id = 6,
             name = "Buah Mangga",
+            search = "mangga",
             description = "Ini adalah buah mangga guys",
             image = R.drawable.mangga
         ),
         RecommendationModel(
             id = 7,
             name = "Buah Jambu",
+            search = "jambu",
             description = "Ini adalah buah jambu guys",
             image = R.drawable.jambu
         ),
         RecommendationModel(
             id = 8,
             name = "Buah Jeruk Bali",
+            search = "jeruk",
             description = "Ini adalah buah jeruk bali guys",
             image = R.drawable.jeruk
         ),
