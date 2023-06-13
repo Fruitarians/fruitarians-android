27294341c34c16001bd73c6c14bda7a5d2d1d4b6
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2023-06-13 19:12:47 +0700

    Set status bar color to green.

--- app/src/main/java/com/althaaf/fruitarians/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/althaaf/fruitarians/ui/profile/ProfileFragment.kt
@@ -1,13 +1,12 @@
 package com.althaaf.fruitarians.ui.profile
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
@@ -46,6 +45,10 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val window: Window? = activity?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.green)
+
         setupViewModel()
         setupGetDataUser()
         setupAction()
@@ -91,8 +94,8 @@ class ProfileFragment : Fragment() {
                             binding.lottieLoading.visibility = View.GONE
                             binding.nameProfile.text = response.data.data.name
                             binding.roleProfile.text = "User"
+                            binding.imgProfile.elevation = 0F
                             binding.imgProfile.setImageDrawable(ContextCompat.getDrawable(binding.imgProfile.context, R.drawable.default_user_biasa))
-                            binding.imgProfile.elevation = 6F
                         } else if (roleUser == "toko") {
                             userDataProfile = Data(
                                 bergabung = response.data.data.bergabung,
